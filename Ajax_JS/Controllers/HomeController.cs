49566bfc03a7abaddf32e422312f2490dc4e8655
sergey.ermolaev <sergey.ermolaev@base4web.ru>
2018-08-06 18:50:53 +0300

    qq

--- Ajax_JS/Controllers/HomeController.cs
+++ Ajax_JS/Controllers/HomeController.cs
@@ -9,15 +9,93 @@ namespace Ajax_JS.Controllers
 {
     public class HomeController : Controller
     {
-        BookContext db = new BookContext();
-        public ActionResult Index()
+        static ChatModel chatModel;
+
+        public ActionResult Index(string user,bool? logOn,bool? logOff,string chatMessage)
         {
-            return View();
+            try
+            {
+                if(chatModel==null)
+                {
+                    chatModel = new ChatModel();
+                }
+
+                if(chatModel.Messages.Count>20)
+                {
+                    chatModel.Messages.RemoveRange(0, 10);
+                }
+
+                if (!Request.IsAjaxRequest())
+                {
+                    return View(chatModel);
+                }
+                else if (logOn != null && (bool)logOn)
+                {
+                    if (chatModel.Users.FirstOrDefault(u => u.Name == user) != null)
+                    {
+                        throw new Exception("такой пользователь уже есть");
+                    }
+                    else if (chatModel.Users.Count > 10)
+                    {
+                        throw new Exception("В чате слишком много людей");
+                    }
+                    else
+                    {
+                        chatModel.Users.Add(new ChatUser()
+                        {
+                            Name = user,
+                            LoginTime = DateTime.Now,
+                            LastPing = DateTime.Now
+                        });
+
+
+                        chatModel.Messages.Add(new ChatMessage()
+                        {
+                            Text = user + "вошел в чат",
+                            Date = DateTime.Now
+                        });
+                    }
+                    return PartialView("ChatRoom", chatModel);
+                }
+                else if (logOff != null && (bool)logOff)
+                {
+                    LogOff(chatModel.Users.FirstOrDefault(u => u.Name == user));
+                    return PartialView("ChatRoom", chatModel);
+                }
+                else
+                {
+                    ChatUser currentUser = chatModel.Users.FirstOrDefault(u => u.Name == user);
+                    currentUser.LastPing = DateTime.Now;
+
+                    if (!string.IsNullOrEmpty(chatMessage))
+                    {
+                        chatModel.Messages.Add(new ChatMessage()
+                        {
+                            User = currentUser,
+                            Text = chatMessage,
+                            Date = DateTime.Now
+                        });
+                    }
+
+                    return PartialView("History", chatModel);
+                }
+                                
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                return Content(ex.Message);
+            }
         }
-        public ActionResult BookSearch(string name)
+        
+        public void LogOff(ChatUser user)
         {
-            var allbooks = db.Books.Where(a => a.Author.Contains(name)).ToList();
-            return PartialView(allbooks);
+            chatModel.Users.Remove(user);
+            chatModel.Messages.Add(new ChatMessage()
+            {
+                Text = user.Name + "покинул чат.",
+                Date = DateTime.Now
+            });
         }
     }
 }
---
+++ Ajax_JS/Models/ChatModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ajax_JS.Models
+{
+    public class ChatModel
+    {
+        public List<ChatUser> Users;
+        public List<ChatMessage> Messages;
+
+        public ChatModel()
+        {
+            Users = new List<ChatUser>();
+            Messages = new List<ChatMessage>();
+            Messages.Add(new ChatMessage()
+            {
+                Text = "Чат запущен" + DateTime.Now
+            });
+        }
+    }
+
+    public class ChatUser
+    {
+        public string Name;
+        public DateTime LoginTime;
+        public DateTime LastPing;
+    }
+
+    public class ChatMessage
+    {
+        public ChatUser User;
+        public DateTime Date = DateTime.Now;
+        public string Text = "";
+    }
+}
